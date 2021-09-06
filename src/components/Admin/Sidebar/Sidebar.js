cef4fdfe3626c174863c0c72edb7914f8846199f
unknown <mhsagar.dev@gmail.com>
2021-09-06 11:50:53 +0600

    bug fixed

--- src/components/Admin/Sidebar/Sidebar.js
+++ src/components/Admin/Sidebar/Sidebar.js
@@ -9,42 +9,64 @@ import './Sidebar.css'
 const Sidebar = () => {
     const [loggedInUser, setLoggedInUser] = useContext(LoggedContext);
     const [isAdmin, setIsAdmin] = useState(false);
+    console.log(isAdmin);
 
-    useEffect(()=>{
-        fetch('https://localhost:5000/isAdmin',{
+    useEffect(() => {
+        fetch('https://salty-gorge-93542.herokuapp.com/isAdmin', {
             method: 'POST',
-            headers:{'Content-Type' : 'Application/json'},
-            body :JSON.stringify({email : loggedInUser.email})
+            headers: { 'Content-Type': 'Application/json' },
+            body: JSON.stringify({ email: loggedInUser.email })
         })
-        .then(res => res.json())
-        .then(data=>setIsAdmin(data))
-    },[loggedInUser])
+            .then(res => res.json())
+            .then(data => {setIsAdmin(data)
+            console.log(data);})
+
+
+    }, [loggedInUser])
     return (
         <div className="sidebar d-flex flex-column justify-content-between col-md-2 py-5 px-4" style={{ height: "100vh" }}>
-            <ul className="list-unstyled">
-                <li>
-                    <Link to="/dashboard/order/:id" className="text-white">
-                        <span>Order</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/dashboard/addReview" className="text-white">
-                        <span>Your Review</span>
-                    </Link>
-                </li>
-                {isAdmin && <div>
-                    <li>
-                        <Link to="/dashboard/addServices" className="text-white">
-                            <span>Add Course</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/dashboard/addAdmin" className="text-white">
-                            <span>Add Admin</span>
-                        </Link>
-                    </li>
-                    </div>}
+            <ul className="list-unstyled"> {
+                isAdmin ?
+    
+                    <div>
+                        <li>
+                            <Link to="/dashboard/addServices" className="text-white">
+                                <span>Add Course</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/dashboard/addAdmin" className="text-white">
+                                <span>Add Admin</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/dashboard/order/:id" className="text-white">
+                                <span>Order</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/dashboard/addReview" className="text-white">
+                                <span>Your Review</span>
+                            </Link>
+                        </li>
+                    </div>
+                    :
+                    <div>
+                        <li>
+                            <Link to="/dashboard/order/:id" className="text-white">
+                                <span>Order</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/dashboard/addReview" className="text-white">
+                                <span>Your Review</span>
+                            </Link>
+                        </li>
+                    </div>
+
+            }
             </ul>
+
             <div>
                 <Link to="/" className="text-white"> <span>Logout</span></Link>
             </div>
